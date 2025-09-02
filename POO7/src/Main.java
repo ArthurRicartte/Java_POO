53974e1f654019647e8d342b85675c56f04e98fb
ArthurRicartte <a.ricartte@gmail.com>
2025-09-01 22:37:16 -0300

    Dell Antigo
    Testando commit no meu nootebook guerreiro.

--- POO7/src/Main.java
+++ POO7/src/Main.java
@@ -38,7 +38,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String msg = JOptionPane.showInputDialog("Quantas pessoas vc deseja listar?");
+        String msg = JOptionPane.showInputDialog("Quantas pessoas vc deseja listar?TO NO DELL");
         int quantidade = Integer.parseInt(msg);
         Pessoa[] pessoas = new Pessoa[quantidade];
 
